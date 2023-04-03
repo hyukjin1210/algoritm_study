54018d87a3b89e091aaacab5e79d85ce3c36b183
hyukjin jang <chaos2061@naver.com>
2023-04-04 01:13:34 +0900

    백준 문제풀이

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,21 +1,19 @@
 package baekjoon;
 
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        int H = sc.nextInt();
+        int M = sc.nextInt();
 
-        if (A > B) {
-            System.out.println(">");
-        }
-        if (A < B) {
-            System.out.println("<");
-        }
-        if (A == B) {
-            System.out.println("==");
-        }
+//        if (M - 45 <= 0 && 0 < H) {
+//            System.out.println(H -1 +" " +(60 +(M-45)));
+//        } else if (M - 45 <= 0 && H == 0) {
+//            H = 23;
+//            System.out.println(H + " " +(60 +(M-45)));
+//        }
     }
 }
