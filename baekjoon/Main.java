91150c6826b643dd0fa26b09b736b101d2545cf8
hyukjin jang <chaos2061@naver.com>
2023-04-09 22:16:02 +0900

    백준 문제풀이

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -6,14 +6,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int H = sc.nextInt();
-        int M = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        int max;
 
-//        if (M - 45 <= 0 && 0 < H) {
-//            System.out.println(H -1 +" " +(60 +(M-45)));
-//        } else if (M - 45 <= 0 && H == 0) {
-//            H = 23;
-//            System.out.println(H + " " +(60 +(M-45)));
-//        }
+        if (x >= y && x >= z) {
+            max = x;
+        } else if (y >= x && y >= z) {
+            max = y;
+        } else {
+            max = z;
+        }
+
+
+        if (x == z && y == z) {
+            System.out.println(10000 + (x * 1000));
+        } else if (x == y || x == z) {
+            System.out.println(1000 + (x * 100));
+        } else {
+            System.out.println(max * 100);
+        }
+
     }
 }
