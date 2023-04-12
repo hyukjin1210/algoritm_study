5f86ac77291a4cb0e436fddc7524133e4881b28d
hyukjin jang <chaos2061@naver.com>
2023-04-12 23:31:17 +0900

    문제풀이

--- baekjoon/Lv_1/boj10950.java
+++ baekjoon/Lv_1/boj10950.java
@@ -1,8 +1,8 @@
-package baekjoon;
+package baekjoon.Lv_1;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class boj10950 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
