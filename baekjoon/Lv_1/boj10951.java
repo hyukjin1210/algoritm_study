870b1379dce668e837d2a10d461838966ada12e7
hyukjin jang <chaos2061@naver.com>
2023-04-18 22:29:44 +0900

    백준 문제풀이

---
+++ baekjoon/Lv_1/boj10951.java
@@ -0,0 +1,26 @@
+package baekjoon.Lv_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader( System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
+
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if (A == 0 && B == 0) {
+                break;
+            }
+            sb.append(A + B).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
