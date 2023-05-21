c0e552f0f0b36c834a1331c40b6df58e9b676f59
Hyuk Jin Jang <chaos2061@naver.com>
2023-05-21 21:02:21 +0900

    백준 문제풀이

---
+++ baekjoon/Lv_1/boj2743.java
@@ -0,0 +1,14 @@
+package baekjoon.Lv_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        int count = S.length();
+        System.out.println(count);
+    }
+}
