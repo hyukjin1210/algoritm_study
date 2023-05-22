7f328aa54c3668449318a9bfde8d36b502a2366c
Hyuk Jin Jang <chaos2061@naver.com>
2023-05-22 10:09:12 +0900

    백준 문제풀이

---
+++ baekjoon/Lv_1/boj11654.java
@@ -0,0 +1,16 @@
+package baekjoon.Lv_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        char c = br.readLine().charAt(0);
+        System.out.println((int) c);
+    }
+}
