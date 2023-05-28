27dc6813c782cd762118eb2828bc5d383f8f696c
Hyuk Jin Jang <chaos2061@naver.com>
2023-05-28 20:21:04 +0900

    Leetcode 문제풀이

---
+++ Leetcode/easy/twoSum.java
@@ -0,0 +1,39 @@
+package Leetcode.easy;
+
+import Leetcode.Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =  new StringTokenizer(br.readLine(), " ");
+
+        int [] nums = new int[st.countTokens()];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int target = Integer.parseInt(br.readLine());
+
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(nums, target);
+
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -7,13 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-//        StringTokenizer st =  new StringTokenizer(br.readLine(), " ");
+//        StringBuilder sb = new StringBuilder();
+        StringTokenizer st =  new StringTokenizer(br.readLine(), " ");
 
-        String str = br.readLine().toUpperCase();
-        System.out.println("대문자 변환" + str);
-        String [] arr = str.split("");
-        System.out.println(Arrays.toString(arr));
+        int [] nums = new int[Integer.parseInt(String.valueOf(st.countTokens()))];
+        int target = Integer.parseInt(br.readLine());
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]  = Integer.parseInt(st.nextToken());
+        }
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] + nums[i+ 1] == target) {
+                System.out.println("[" + i + ", " + (i +1) + "]");
+                break;
+            }
+        }
     }
 }
