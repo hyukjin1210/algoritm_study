82ab8200b3b6fc5c57037cc1cf20947453b1a789
Hyuk Jin Jang <chaos2061@naver.com>
2023-06-28 23:22:15 +0900

    알고리즘 문제풀이

---
+++ note/Reverse2.java
@@ -0,0 +1,36 @@
+package note;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Reverse2 {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] str = new String[N];
+        for (String x : str) {
+            char[] s = x.toCharArray();
+        }
+    }
+}
